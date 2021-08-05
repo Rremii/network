ac23c996d056aa52315309fcedea25eef31eafb5
Remi <noruto2025@ya.ru>
2021-08-05 09:52:54 +0300

    qwe

--- src/components/Hoc/withAuthRedirect.js
+++ src/components/Hoc/withAuthRedirect.js
@@ -2,27 +2,26 @@ import {Redirect} from "react-router-dom";
 import React from "react";
 import {connect} from "react-redux";
 
-let mapStateToProps = (state) => {
-    return {
-        isAuth: state.auth.isAuth,
-        initialized: state.app.initialized
-    }
-}
-
 const withAuthRedirect = (Component) => {
     class redirectComponent extends React.Component {
         render() {
-            if (!this.props.isAuth) {
-                return <Redirect to={"/login"}/>
-            } else return <Component {...this.props} />
 
+            if (!this.props.isAuth) {
+                alert('you should login first!')
+                return <Redirect to={"/login"}/>
+            }
+            return <Component {...this.props} />
         }
     }
 
+    let mapStateToProps = (state) => {
+        return {
+            isAuth: state.auth.isAuth
+        }
+    }
 
     let connectedAuthRedirectComponent = connect(mapStateToProps)(redirectComponent)
 
     return connectedAuthRedirectComponent
 }
 export default withAuthRedirect
-
